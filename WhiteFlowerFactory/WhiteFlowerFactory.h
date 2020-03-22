72fb8476decff0b387d614e5cb0d8ad3b3e6cdc8
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro-2.local>
2020-03-22 13:18:35 +0700

    adding mac support

--- WhiteFlowerFactory/WhiteFlowerFactory.h
+++ WhiteFlowerFactory/WhiteFlowerFactory.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Stephen Bodnar. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for WhiteFlowerFactory.
 FOUNDATION_EXPORT double WhiteFlowerFactoryVersionNumber;
