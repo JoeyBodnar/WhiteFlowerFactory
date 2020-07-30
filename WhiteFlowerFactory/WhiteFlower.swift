8b8fb533ecd5901717c3bf3f9786e094fd5ba90f
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro-2.local>
2020-07-30 21:08:41 +0700

    mock data task

--- WhiteFlowerFactory/WhiteFlower.swift
+++ WhiteFlowerFactory/WhiteFlower.swift
@@ -8,14 +8,23 @@
 
 import Foundation
 
-public protocol URLSessionProtocol {
-    
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+public protocol URLSessionDataTaskProtocol {
+    func resume()
 }
 
-extension URLSession: URLSessionProtocol { }
+extension URLSessionDataTask: URLSessionDataTaskProtocol { }
 
 public typealias DataTaskCompletion = (APIResponse) -> Void
+
+public protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+extension URLSession: URLSessionProtocol {
+    public func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return (self.dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask) as URLSessionDataTaskProtocol
+    }
+}
 
 public final class WhiteFlower {
     
