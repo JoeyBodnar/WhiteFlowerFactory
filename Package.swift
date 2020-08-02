6d65d8b084751ad2542c78da2a101d1ab73213de
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro-2.local>
2020-08-02 13:12:36 +0700

    add platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "WhiteFlowerFactory",
+    platforms: [.iOS(SupportedPlatform.IOSVersion.v11), .macOS(SupportedPlatform.MacOSVersion.v10_12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
